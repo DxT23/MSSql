db04553778ea053002b3452cd64e0d25013ffff6
DxT23 <enwongoabasi15@gmail.com>
2024-10-20 17:20:10 +0200

    Add files via upload

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,19 +1,15 @@
-/*a) Query the relevant tables to retrieve the year of invoice issuance and the genre of the track(s) sold on the invoice.
-b) Summarize the calculated net value of invoice items and quantity in separate columns based on the year and genre.
-c) Include only those tracks that appear on at least one playlist.
-d) Exclude tracks that do not have a composer.
-e) Include in the list only tracks that are longer than the average length of tracks in the same genre.
-f.) Create a yearly recurring ranking in a new column for the list. Base the ranking on the total quantity sold (higher quantity results in a better ranking). The ranking should follow the Olympic-style format.
-67 rows
-Last row:
-2013	Electronica/Dance	0.99	1	10*/
-
-select YEAR(invoicedate), g.name, Sum(il.unitprice * il.quantity), sum(quantity),
-Rank() over (partition by year(invoicedate) order by sum(quantity) desc)
-from invoice i join invoiceline il on i.invoiceid = il.invoiceid
-join track t on il.trackid = t.trackid
-join genre g on t.genreid = g.genreid
-where t.trackid in (select trackid from playlisttrack)
-and composer is not null
-and Milliseconds > (select avg(milliseconds) from track t2 where t2.genreid = t.genreid)
-group by YEAR(invoicedate), g.name
+/*Individual assignment 1 
+Create a list of albums distributed by the company according to the following criteria: 
+a) The list should include the name of the artist and the title of the album. 
+b) In a new column, calculate the total length of the album's tracks in minutes. The length is given in 
+milliseconds and can be easily converted. Round the result to one decimal place. (Ensure that the 
+milliseconds column is an integer; division by an integer can result in data loss). 
+c) Filter out the rows where the unit price is greater than one dollar. 
+d) In a separate column, indicate how many music tracks are on the album. 
+e) Remove albums from the result that have fewer than 5 music tracks.*/
+select Artist.Name [Artist Name], Album.Title, round(SUM(Milliseconds)/1000/60.00,1) [Total minutes], COUNT(*)
+from Artist join Album on Artist.ArtistId = Album.ArtistId
+join track on track.albumid=album.albumid
+where  UnitPrice <= 1
+group by Artist.Name , Album.Title
+having COUNT(*)>=5
